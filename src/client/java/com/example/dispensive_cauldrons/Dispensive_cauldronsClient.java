ac3b44e1809a39d9602250c904c034b04a03765c
root <root@Vin.localdomain>
2025-10-23 23:25:55 +0100

    initial commit

---
+++ src/client/java/com/example/dispensive_cauldrons/Dispensive_cauldronsClient.java
@@ -0,0 +1,10 @@
+package com.example.dispensive_cauldrons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Dispensive_cauldronsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
